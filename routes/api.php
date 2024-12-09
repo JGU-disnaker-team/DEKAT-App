6e930528179ff145d407dc30d8778be55eadab66
Zamir Mustafid <aryobimo354@gmail.com>
2024-12-09 10:37:28 +0700

    Sorry kebanyakan commit, tadi ada error wkwkwk capekkkk

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,5 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('login', \App\Http\Controllers\Api\Auth\LoginController::class);
 Route::post('register', \App\Http\Controllers\Api\Auth\RegisterController::class);
