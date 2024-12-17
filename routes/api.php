4c225cd6ca887ccbfc749b781165871fca21b8e7
Zamir Mustafid <aryobimo354@gmail.com>
2024-12-17 09:59:55 +0700

    Edit sedikit

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TodoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', action: [LoginController::class, 'login']);
 
-Route::apiResource('todos' , \App\Http\Controllers\Api\TodoController::class)->middleware(middleware: 'auth:sanctum');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::apiResource('todos', TodoController::class);
