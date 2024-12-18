8d0b2fc888ada9f838f7b48cddd77d68dc26f25e
meijibims <167759084+meijibims@users.noreply.github.com>
2024-12-18 11:23:18 +0700

    update css login-admin

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,11 +5,8 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Login Admin</title>
-<<<<<<< Updated upstream
-  <link rel="stylesheet" href="{{ asset('css/login-admin.css')}}" />
-=======
-  <link rel="stylesheet" href="{{ asset('resources/css/login-admin.css') }}">
->>>>>>> Stashed changes
+  <!-- <link rel="stylesheet" href="/DEKAT-App/resources/css/login-admin.css"> -->
+  <link rel="stylesheet" href="/DEKAT-App/public/assets/css/login-admin.css">
   <!-- Font Awesome CDN link for icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
 </head>
