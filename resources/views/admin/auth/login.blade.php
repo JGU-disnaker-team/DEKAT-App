ef56dae4f23f8192e88522f738b23a82d9394374
hellokirana <intaniasyahla@gmail.com>
2025-01-05 16:58:13 +0700

    LOGIN ADMIN BISA YEY

---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,56 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login Admin</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/login-admin.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="title"><span>Admin</span></div>
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+
+            <!-- Email Address -->
+            <div class="row">
+                <i class="fas fa-envelope"></i>
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus autocomplete="username" placeholder="Email" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Password -->
+            <div class="row">
+                <i class="fas fa-lock"></i>
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" placeholder="Kata Sandi" />
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Submit Button -->
+            <div class="row button">
+                <input type="submit" value="Login" />
+            </div>
+
+            <!-- Pesan Error -->
+            @if ($errors->any())
+                <div class="error-messages">
+                    @foreach ($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
+        </form>
+
+        <!-- Forgot Password -->
+        @if (Route::has('password.request'))
+            <div class="forgot-password">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            </div>
+        @endif
+    </div>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+require __DIR__ . '/admin-auth.php';
+
+require __DIR__ . '/mitra-auth.php';
+
+
